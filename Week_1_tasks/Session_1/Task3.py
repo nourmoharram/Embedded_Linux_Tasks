c6ea4242bc1de3571587034d2720b731a490ea17
Nour Moharram <100662212+nourmoharram@users.noreply.github.com>
2023-08-12 22:04:02 +0300

    Delete Week_1_tasks directory

--- Week_1_tasks/Session_1/Task3.py
+++
@@ -1,9 +0,0 @@
-import os
-import platform
-
-# Print operating system information and System Paths
-print("Operating System:", os.name)
-print("System:", platform.system())
-print("Release:", platform.release())
-print("Version:", platform.version())
-print("Paths:",os.environ['PATH'])
